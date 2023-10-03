7e8273c3ca5b8d4b6a6ee928cb790611f007ce06
usuario <usuario@pc21-098>
2023-10-03 17:32:54 +0200

    Pruebas unitarias definidas

---
+++ src/test/EmpleadoTest.java
@@ -0,0 +1,83 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmpleadoTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	void testCalculoNominaBruta_Encargado() {
+		fail("Not yet implemented");
+	}	
+	
+	void testCalculoNominaBruta_Vendedor() {
+		fail("Not yet implemented");
+	}	
+	
+	void testCalculoNominaBruta_Otro() {
+		fail("Not yet implemented");
+	}	
+	
+	@Test
+	void testCalculoNominaBruta_VentaMesNegativa() {
+		fail("Not yet implemented");
+	}
+
+	void testCalculoNominaBruta_Menor1000() {
+		fail("Not yet implemented");
+	}	
+	
+	void testCalculoNominaBruta_Entre1000y1500() {
+		fail("Not yet implemented");
+	}
+	
+	void testCalculoNominaBruta_Mayor1500() {
+		fail("Not yet implemented");
+	}
+	
+	void testCalculoNominaBruta_HorasPositivas() {
+		fail("Not yet implemented");
+	}
+	
+	void testCalculoNominaBruta_HorasNegativas() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaNeta_Negativa() {
+		fail("Not yet implemented");
+	}
+	
+	void testCalculoNominaNeta_Menor2100() {
+		fail("Not yet implemented");
+	}
+	
+	void testCalculoNominaNeta_Entre2100y2500() {
+		fail("Not yet implemented");
+	}
+	
+	void testCalculoNominaNeta_Mayor2500() {
+		fail("Not yet implemented");
+	}
+
+}
