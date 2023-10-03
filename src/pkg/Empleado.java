e22220001a7d26b11d64c96d70e121f2847a881d
usuario <usuario@pc21-098>
2023-10-03 17:18:41 +0200

    Formato

--- src/pkg/Empleado.java
+++ src/pkg/Empleado.java
@@ -1,33 +1,31 @@
 package pkg;
 
 enum TipoEmpleado {
-	Vendedor, 
-	Encargado;
+	Vendedor, Encargado;
 }
 
 public class Empleado {
 	public float salario_base, primas, extras;
-	
+
 	public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
-		
+
 		if (tipo == TipoEmpleado.Vendedor) {
 			salario_base = 2000f;
 		} else {
 			salario_base = 2500f;
 		}
-		
-		if(ventasMes >= 1500) {
+
+		if (ventasMes >= 1500) {
 			primas = 200f;
-		}
-		else if (ventasMes >= 1000) {
+		} else if (ventasMes >= 1000) {
 			primas = 100f;
 		}
-		
+
 		extras = horasExtra * 30;
-		
+
 		return salario_base + primas + extras;
 	}
-	
+
 	public float calculoNominaNeta(float nominaBruta) {
 		return 0;
 	}
