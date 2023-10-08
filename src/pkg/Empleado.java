6bf5a99006dd3d4f0b55d49297e6dbd8bedb01d3
Ultra <Ultra@Laptop-Fer>
2023-10-08 12:08:20 +0200

    Salario base

--- src/pkg/Empleado.java
+++ src/pkg/Empleado.java
@@ -6,7 +6,11 @@ enum TipoEmpleado {
 
 public class Empleado {
 	public float salario_base, primas, extras, retencion;
-
+	
+	public Empleado(float salario_base) {
+		this.salario_base = salario_base;
+	}
+	
 	public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
 
 		if (tipo == TipoEmpleado.Vendedor) {
